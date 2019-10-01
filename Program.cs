8b4b0ae1afeb800986b523fb8d89794fa0d68cd9
MaisemShah <54338906+MaisemShah@users.noreply.github.com>
2019-10-01 12:45:47 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace FFirstApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
+    class candy
+    {
+        public void hashbrowns()
+        {
+            Console.WriteLine("123 I know it me");
+            Console.WriteLine("do u like hashbrowns with hocalate");
+        }
+        int hyena = 32; 
+    }
+}
